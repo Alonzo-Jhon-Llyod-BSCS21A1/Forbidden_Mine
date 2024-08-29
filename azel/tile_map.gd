47a65379eb75f8d2474d457209577cf266d662d1
Jhune Rey E. Magdurulan <c2014-23@itmlyceumalabang.onmicrosoft.com>
2024-08-29 14:55:52 +0800

    Dungeon Genarator nalang galit si boss ivan e

--- azel/tile_map.gd
+++ azel/tile_map.gd
@@ -14,9 +14,7 @@ var loaded_chunks = []
 func _ready():
 	moisture.seed = randi()
 	temperature.seed = randi()
-	altitude.seed = randi()
 	
-	altitude.frequency = 0.01
 	var player_tile_pos = local_to_map(player.position)
 	generate_chunk(player_tile_pos)
 	
@@ -24,14 +22,14 @@ func generate_chunk(pos):
 	for x in range(width):
 		for y in range(height):
 			
-			var moist = moisture.get_noise_2d(pos.x - (width/2) + x, pos.y - (height/2) + x) * 10
-			var temp = temperature.get_noise_2d(pos.x - (width/2) + x, pos.y - (height/2) + x) * 10
-			var alt = altitude.get_noise_2d(pos.x - (width/2) + x, pos.y - (height/2) + x) * 10
+			var moist = moisture.get_noise_2d((width/2) + x,(height/2) + x) * 10
+			var temp = temperature.get_noise_2d((width/2) + x,(height/2) + x) * 10
+			var alt = pos.y - (height/2) + y
 			
-			if alt < 0:
-				set_cell(0, Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y),0, Vector2(6, round(2*(temp + 10)/ 20)))
+			if alt > 0:
+				set_cell(0, Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y),0, Vector2(6, 2))
 			else:
-				set_cell(0, Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y),0, Vector2(round(6*(moist + 10)/ 20), round(2*(temp + 10)/ 20)))
+				set_cell(0, Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y),0, Vector2(4, 1))
 			
 			if Vector2(pos.x, pos.y) not in loaded_chunks:
 				loaded_chunks.append(Vector2i(pos.x, pos.y))
