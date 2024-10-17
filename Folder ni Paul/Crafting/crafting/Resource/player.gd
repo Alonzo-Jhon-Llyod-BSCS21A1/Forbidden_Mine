23745757d69ba1adec92d6d39c445b828573c1c8
Marasigan-Paul-Foster-BSCS21A1 <c1115-23@itmlyceumalabang.onmicrosoft.com>
2024-10-17 20:30:14 +0800

    Add files via upload

---
+++ Folder ni Paul/Crafting/crafting/Resource/player.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+func _physics_process(delta):
+	velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down")*200
+	move_and_slide()
+	
+func add_item(item):
+	var inventory = get_tree().current_scene.find_child("Inventory")
+	inventory.add_item(item)
