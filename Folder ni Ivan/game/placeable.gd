588f01f3d01a0e0b9a42cb840742b3730813f49f
Jhune Rey E. Magdurulan <c2014-23@itmlyceumalabang.onmicrosoft.com>
2024-10-18 10:09:05 +0800

    DeletedFolder

--- Folder ni Ivan/game/placeable.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func place_block(position: Vector2):
-	self.position = position
-	get_parent().add_child(self)
-	
-func break_block():
-	# Optional: Play a breaking animation or sound here
-	queue_free()  # Remove the block from the scene
