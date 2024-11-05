f751a531e2b5b6021dc5eafe27d561f22f638b17
Marasigan-Paul-Foster-BSCS21A1 <c1115-23@itmlyceumalabang.onmicrosoft.com>
2024-11-05 12:05:42 +0800

    craft shiut

---
+++ forbiddenmine/craftings/Resource/recipe.gd
@@ -0,0 +1,30 @@
+extends HBoxContainer
+
+@onready var craft = $craft 
+@export var item : crafttest = null
+@onready var recipe = item.recipe
+
+func _ready():
+	craft.icon = item.icon
+	
+	for i in range(recipe.size()):
+		get_child(i).item = recipe[i]
+
+func check():
+	var flag = []
+		
+	for i in range(recipe.size()):
+		flag.append(get_child(i).check())
+		
+	if false not in flag:
+		craft.disabled = false
+	
+func _on_craft_pressed():
+	craft.disabled = true
+	var inventory = get_tree().current_scene.find_child("Inventory")
+	
+	for i in recipe:  
+		inventory.remove_item(i)
+	inventory.add_item(item)
+	
+	
