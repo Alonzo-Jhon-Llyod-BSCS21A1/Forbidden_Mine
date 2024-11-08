deb4ded6329190595f31931220769ee0cfa70472
Jhune Rey E. Magdurulan <c2014-23@itmlyceumalabang.onmicrosoft.com>
2024-11-08 17:11:11 +0800

    Enemies

--- forbiddenmine/Spectator.gd
+++ forbiddenmine/Spectator.gd
@@ -1,28 +1,19 @@
 extends CharacterBody2D
-const  NAME = "Enemy"
+#const  NAME = "Enemy"
 signal enemy_attack
 @onready var character_body_2d: CharacterBody2D = $"."
 
-const SPEED = 50
+const SPEED = 200
 const GRAVITY = 500  # Adjust this value for gravity strength
 const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
-	# Apply gravity to the enemy's vertical velocity
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta
-
 	# Move towards the player's position
 	if GlobalVar.charposition != null:
 		if position.distance_to(GlobalVar.charposition) > 1:
 			var direction = (GlobalVar.charposition - position).normalized()
+			#velocity = direction * SPEED
 			velocity.x = direction.x * SPEED
 			$AnimatedSprite2D.flip_h =direction.x < 0
-			# Control vertical movement towards the player if needed
-			if is_on_wall() and is_on_floor():
-				velocity.y = -250
-	#for body in $HitBox.get_overlapping_bodies():
-		#if body.get("NAME")== "Player":
-			#emit_signal("enemy_attack")
 						
 	move_and_slide()
--- forbiddenmine/character_body_2d.gd
+++ forbiddenmine/character_body_2d.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
+const  NAME = "Player"
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var health_bar: ProgressBar = $"../CanvasLayer/HealthBar"
+@onready var character_body_2d: CharacterBody2D = $"."
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
@@ -17,6 +19,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		animated_sprite_2d.animation = "fall"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("up") and is_on_floor():
@@ -34,5 +37,14 @@ func _physics_process(delta):
 		animated_sprite_2d.flip_h = true
 	if Input.is_action_just_pressed("right"):
 		animated_sprite_2d.flip_h = false
+		
+	for body in $Area2D.get_overlapping_bodies():
+		if body.get("NAME") == "Enemy":
+			print("tinamaain")
+			knock_back(body.velocity)
 	move_and_slide()
+	
+func knock_back(enemyVelocity : Vector2):
+	var knock_back_direction = (enemyVelocity - velocity).normalized() * 200
+	velocity.x = knock_back_direction.x
 		
